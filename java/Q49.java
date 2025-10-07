88541433f89de29006b80ac36a51fd2eb276caaf
Master 369 <147631040+prathap929master@users.noreply.github.com>
2025-10-07 14:38:26 +0530

    Update Q49.java

--- java/Q49.java
+++ java/Q49.java
@@ -1,28 +1,87 @@
-#include <iostream>
-using namespace std;
+import java.util.Scanner;
 
-class TIME {
-    int h, m, s;
-public:
-    TIME() {}
-    TIME(int hh,int mm,int ss){ h=hh; m=mm; s=ss; }
-    void display(){ cout << h << ":" << m << ":" << s << endl; }
-    TIME add(TIME t) {
-        TIME temp;
-        temp.s = s + t.s;
-        temp.m = m + t.m + temp.s/60;
-        temp.h = h + t.h + temp.m/60;
-        temp.s %= 60;
-        temp.m %= 60;
-        return temp;
+public class AddTimes {
+    public static void main(String[] args) {
+        // Create two Time objects
+        Time time1 = new Time();
+        Time time2 = new Time();
+
+        // Read time values from the user
+        System.out.println("--- Enter Time 1 ---");
+        time1.readTime();
+        System.out.println("\n--- Enter Time 2 ---");
+        time2.readTime();
+
+        // Add the two time objects
+        Time sumTime = Time.add(time1, time2);
+
+        // Display the results
+        System.out.println("\n--- Displaying Time Objects ---");
+        System.out.print("Time 1: ");
+        time1.displayTime();
+        System.out.print("Time 2: ");
+        time2.displayTime();
+        System.out.print("Sum of times: ");
+        sumTime.displayTime();
     }
-};
+}
 
-int main() {
-    TIME t1(2,45,50), t2(1,20,30);
-    TIME t3 = t1.add(t2);
-    cout << "Time1: "; t1.display();
-    cout << "Time2: "; t2.display();
-    cout << "Sum:   "; t3.display();
-    return 0;
+// The Time class, which is not public, can be in the same file.
+class Time {
+    int hours;
+    int minutes;
+    int seconds;
+
+    // No-argument constructor to initialize time to 00:00:00
+    public Time() {
+        this.hours = 0;
+        this.minutes = 0;
+        this.seconds = 0;
+    }
+
+    // Parameterized constructor to initialize with given values
+    public Time(int hours, int minutes, int seconds) {
+        this.hours = hours;
+        this.minutes = minutes;
+        this.seconds = seconds;
+    }
+
+    // Method to read time from user input
+    public void readTime() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter time (hours, minutes, seconds):");
+        System.out.print("Hours: ");
+        this.hours = scanner.nextInt();
+        System.out.print("Minutes: ");
+        this.minutes = scanner.nextInt();
+        System.out.print("Seconds: ");
+        this.seconds = scanner.nextInt();
+    }
+
+    // Method to display the time in HH:MM:SS format
+    public void displayTime() {
+        System.out.printf("%02d:%02d:%02d%n", this.hours, this.minutes, this.seconds);
+    }
+
+    // Static method to add two Time objects
+    public static Time add(Time t1, Time t2) {
+        Time sum = new Time();
+        sum.seconds = t1.seconds + t2.seconds;
+        sum.minutes = t1.minutes + t2.minutes;
+        sum.hours = t1.hours + t2.hours;
+
+        // Handle overflows for seconds
+        if (sum.seconds >= 60) {
+            sum.minutes += sum.seconds / 60;
+            sum.seconds %= 60;
+        }
+
+        // Handle overflows for minutes
+        if (sum.minutes >= 60) {
+            sum.hours += sum.minutes / 60;
+            sum.minutes %= 60;
+        }
+
+        return sum;
+    }
 }
