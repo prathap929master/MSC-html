88ee7e8d13a4e712e899459980dc682b40675cc8
Master 369 <147631040+prathap929master@users.noreply.github.com>
2025-10-09 18:21:02 +0530

    Update Q50.java

--- java/Q50.java
+++ java/Q50.java
@@ -1,22 +1,40 @@
-class VolumeCalc {
-    double volume(double a) {                // cube
-        return a*a*a;
-    }
-    double volume(double r, double h) {      // cylinder
-        return Math.PI * r * r * h;
-    }
-    double volume(double l, double b, double h) { // rectangle
-        return l * b * h;
-    }
-    double volumeSphere(double r) {          // sphere
-        return (4.0/3.0) * Math.PI * r * r * r;
+// Program to find volume of Cube, Cylinder and Sphere using Method Overloading
+import java.util.Scanner;
+
+class VolumeCalculator {
+
+    // Volume of cube: a^3
+    double volume(double a) {
+        return a * a * a;
     }
 
+    // Volume of cylinder: π * r^2 * h
+    double volume(double r, double h) {
+        return Math.PI * r * r * h;
+    }
+
+    // Volume of sphere: (4/3) * π * r^3
+    double volume(float r) {
+        return (4.0 / 3.0) * Math.PI * r * r * r;
+    }
+}
+
+public class VolumeOverload {
     public static void main(String[] args) {
-        VolumeCalc v = new VolumeCalc();
-        System.out.println("Cube : " + v.volume(3));
-        System.out.println("Cylinder : " + v.volume(2,5));
-        System.out.println("Rectangle : " + v.volume(2,3,4));
-        System.out.println("Sphere : " + v.volumeSphere(3));
+        Scanner sc = new Scanner(System.in);
+        VolumeCalculator obj = new VolumeCalculator();
+
+        System.out.print("Enter side of cube: ");
+        double a = sc.nextDouble();
+        System.out.println("Volume of Cube = " + obj.volume(a));
+
+        System.out.print("\nEnter radius and height of cylinder: ");
+        double r = sc.nextDouble();
+        double h = sc.nextDouble();
+        System.out.println("Volume of Cylinder = " + obj.volume(r, h));
+
+        System.out.print("\nEnter radius of sphere: ");
+        float rs = sc.nextFloat();
+        System.out.println("Volume of Sphere = " + obj.volume(rs));
     }
 }
